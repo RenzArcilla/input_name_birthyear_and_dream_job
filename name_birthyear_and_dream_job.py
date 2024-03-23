8cd856909802668a5931e4273cc04258351530fc
unknown <renzarcilla09@gmail.com>
2024-03-23 19:04:58 +0800

    wrote pseudocode

---
+++ name_birthyear_and_dream_job.py
@@ -0,0 +1,8 @@
+# pseudocode
+
+# ask user to input name
+# ask user to input birth year
+# ask user to input dream job
+# merge name and birth year into 1 string
+# create window
+# display name, birthyear, and dreamjob in window
