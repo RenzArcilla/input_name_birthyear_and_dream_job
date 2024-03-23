9e16af7ee608c5176393f8feb4bdb8c4570a580b
unknown <renzarcilla09@gmail.com>
2024-03-23 19:15:30 +0800

    wrote code for dreamjob input

--- name_birthyear_and_dream_job.py
+++ name_birthyear_and_dream_job.py
@@ -7,6 +7,8 @@ user_name = input("Please enter your name: ")
 user_birth_year = input("Please enter your birth year: ")
 
 # ask user to input dream job
+user_dream_job = input("Please enter your dream job: ")
+
 # merge name and birth year into 1 string
 # create window
 # display name, birthyear, and dreamjob in window
