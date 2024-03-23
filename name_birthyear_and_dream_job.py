acb1944d58b0fe5e2470aa212141893dfdaddad4
unknown <renzarcilla09@gmail.com>
2024-03-23 19:08:50 +0800

    wrote code for name input

--- name_birthyear_and_dream_job.py
+++ name_birthyear_and_dream_job.py
@@ -1,6 +1,8 @@
 # pseudocode
 
 # ask user to input name
+user_name = input("Please enter your name: ")
+
 # ask user to input birth year
 # ask user to input dream job
 # merge name and birth year into 1 string
